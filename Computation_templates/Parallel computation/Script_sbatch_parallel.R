29e9686a4c740b1edc668f0b7dfabad7678d664a
= <=>
2025-04-15 17:16:35 +0200

    Reorganization of the folder

--- Computation_templates/Parallel computation/Script_sbatch_parallel.R
+++ Computation_templates/Parallel computation/Script_sbatch_parallel.R
@@ -1,15 +1,45 @@
-setwd("/Volumes/diangelantonio/users/alessia_mapelli/conditional_neurofgm/Simulation_studies")
 rm(list=ls(all=TRUE))
+time.start <- Sys.time()
 
-time.start.tot <- Sys.time()
+#################################################
+## 0. USER DEFINED PARAMETERS (MODIFY THIS PART)
+#################################################
+score_path = ""
+grouping_path = ""
+output_path = ""
+name_output = ""
+n_basis = 
+L = 100
+K = 5
+thres.ctrl = c(0, 0.2, 0.4, 0.8, 1.2, 1.6, 2.0)
+tol.abs =1e-4
+tol.rel = 1e-4
+eps = 1e-08
+verbose = FALSE
+p.rand.lam = 0.5
+p.rand.thr = 1
+##############################################
 
-data.path <- "/Volumes/diangelantonio/users/alessia_mapelli/Brain_simulations/Sim_1"
-func.path <- "/Volumes/diangelantonio/users/alessia_mapelli/conditional_neurofgm/Graph_estimation"
-save.path <- "/Volumes/diangelantonio/users/alessia_mapelli/Brain_simulations/Sim_1"
-runtime.path <- "/Volumes/diangelantonio/users/alessia_mapelli/Brain_simulations/Sim_1"
+#################################################
+## Include parameter info in the logs
+cat("Score source: ", score_path,"\n")
+cat("Grouping source: ", grouping_path,"\n")
+cat("Parameters: \n")
+cat("n_basis: ", n_basis ,"\n")
+cat("L: ", L ,"\n")
+cat("K : ", K  ,"\n")
+cat("thres.ctrl : ", thres.ctrl  ,"\n")
+cat("p.rand.lam:", p.rand.lam, "\n")
+cat("p.rand.thr:", p.rand.thr, "\n")
+#################################################
 
-scores <- read.csv(paste(data.path, "freq_scores_reord.csv", sep ="/"))[, -1]
-n_basis <- 8
+
+#############################################
+###### 1. Upload all the data needed
+#############################################
+args <- commandArgs(trailingOnly = TRUE)
+j = as.numeric(args[[1]])
+scores <- read.csv(score_path)[, -1]
 n_nodes <- ncol(scores)/n_basis
 n_samples <- nrow(scores)
 names <- rep(NA, ncol(scores))
@@ -17,11 +47,14 @@ for(l in 1:ncol(scores)){
   names[l] <- paste("f",ceiling(l/n_basis) ,".",l%%n_basis, sep ="")
 }
 colnames(scores) <- names
-covariates <- data.frame(group= c(rep(1,24),rep(0,26)))
-covariates$group <- as.factor(covariates$group)
+covariates <- data.frame( group = as.factor(read.csv(grouping_path)[,-1]))
 full_data <- cbind(covariates,scores)
 
-######################## FUNCTIONS NEDDED
+#############################################
+###### 2.Define functions needed for the computation
+#############################################
+
+###### SAME lambda.sup
 lambda.sup <- function(A.X, A.Y){
   n <- nrow(A.X)
   M <- ncol(A.Y)
@@ -35,7 +68,7 @@ lambda.sup <- function(A.X, A.Y){
 }
 
 
-############################# UPDATE grp.soft.thres
+###### UPDATE grp.soft.thres
 grp.soft.thres.two.groups <- function(V.Grp, d, lambda, rho){
   # V.Grp: a group of p matrices (Vk), each dim M * M
   # d: vector, length pM.
@@ -60,7 +93,7 @@ grp.soft.thres.two.groups <- function(V.Grp, d, lambda, rho){
   return(P.Grp)
 }
 
-############################# UPDATE objective.function
+######### UPDATE objective.function
 objective.function.two.groups <- function(A.X, A.Y, d, B, lambda){
   # A.X: matrix, n * 2(p-1)M
   # A.Y: matrix, n * M
@@ -85,7 +118,7 @@ objective.function.two.groups <- function(A.X, A.Y, d, B, lambda){
   return(obj)
 }
 
-############################# UPDATE ADMM.grplasso -> miss a parantesis or one more
+########### UPDATE ADMM.grplasso -> miss a parantesis or one more
 
 ADMM.grplasso.two.groups <- function(A.X, A.Y, d, lambda,
                                      rho.init=1, maxiter=2000, mu=10, tau=2,
@@ -219,7 +252,10 @@ ADMM.grplasso.two.groups <- function(A.X, A.Y, d, lambda,
     obj.func.path <- c(obj.func.path, obj)
   }
   
-  #message("ADMM converged after ", iter, " iterations.")
+  if(iter == maxiter){
+      cat("ADMM did not converge.")
+  }
+  
   
   result <- list(P=P.new, Q=Q.new, U=U.new,
                  prim.res=prim.resid.vec, dual.res=dual.resid.vec,
@@ -228,21 +264,9 @@ ADMM.grplasso.two.groups <- function(A.X, A.Y, d, lambda,
                  obj.func.path=obj.func.path)
   return(result)}
 
-########################## START COMPUTATION
-
-
-
-
-
-
-
-L = 10
-K = 2
-thres.ctrl = c(0, 0.2, 0.4, 0.8, 1.2, 1.6, 2.0)
-tol.abs =1e-4
-tol.rel = 1e-4
-eps = 1e-08
-verbose = TRUE
+##############################################
+##### 3.COMPUTATION
+##########################################
 
 len.t <- length(thres.ctrl)
 n <- nrow(scores)
@@ -270,15 +294,15 @@ temp_groups <- c(0)
 if (verbose) {
   cat("Comuputation of the design matrix \n ")
 }
-for(j in 1:(q + 1)){
-  product <- scores * iU[, j]
-  if (j != 1) {
+for(c in 1:(q + 1)){
+  product <- scores * iU[, c]
+  if (c != 1) {
     original_colnames <- colnames(product)
-    iU_name <- colnames(iU)[j]
+    iU_name <- colnames(iU)[c]
     new_colnames <- paste(iU_name, ":", original_colnames, sep = "")
     colnames(product) <- new_colnames
   }
-  for (i in 1:p){temp_groups <- c(temp_groups, rep(i+(p*(j-1)),M))}
+  for (i in 1:p){temp_groups <- c(temp_groups, rep(i+(p*(c-1)),M))}
   interM <- cbind(interM, product)
 }
 interM <- interM[, -1]
@@ -298,30 +322,32 @@ P.def <- matrix(0, 2*(p-1)*M, M)
 Q.def <- matrix(0.1, 2*(p-1)*M, M)
 U.def <- matrix(0.01, 2*(p-1)*M, M)
 
-G.mat <- matrix(NA, p, (q+1)*p)
+cat(paste("Processing node ", j,"\n"))
+jth.range.y <- (j-1)*M+(1:M)
+A.Y <- as.matrix(interM[, jth.range.y])
+jth.range.x <- c(jth.range.y,(j+p-1)*M+(1:M))
+A.X <- as.matrix(interM[, -jth.range.x])
+groups <- temp_groups[-jth.range.x]
+  
+P <- P.def; Q <- Q.def; U <- U.def
+  
+SCV.mat <- matrix(NA, ceiling(L * p.rand.lam), ceiling(len.t * p.rand.thr))
+#SCV.mat <- matrix(NA, L , len.t )
+  
+lambda.max <- lambda.sup(A.X, A.Y)
+lambdas <- exp(seq(log(lambda.max), log(1e-4), length.out = L))
 
-j=p
-for(j in 1:2){
-  time.start <- Sys.time()
-  cat(paste("Processing node ", j,"\n"))
-  jth.range.y <- (j-1)*M+(1:M)
-  A.Y <- as.matrix(interM[, jth.range.y])
-  jth.range.x <- c(jth.range.y,(j+p-1)*M+(1:M))
-  A.X <- as.matrix(interM[, -jth.range.x])
-  groups <- temp_groups[-jth.range.x]
+if(p.rand.lam == 1){
+  random.sel.lambdas <- lambdas
+} else{
+set.seed(123+10*j)
+random.sel.indexes <- sample(seq_along(lambdas), ceiling(L * p.rand.lam))
+random.sel.lambdas <- lambdas[random.sel.indexes[order(random.sel.indexes)]]
+}
+
+L_random <- length(random.sel.lambdas)
   
-  P <- P.def; Q <- Q.def; U <- U.def
-  
-  SCV.mat <- matrix(NA, ceiling(L * 0.5), ceiling(len.t * 0.5))
-  
-  lambda.max <- lambda.sup(A.X, A.Y)
-  lambdas <- exp(seq(log(lambda.max), log(1e-4), length.out = L))
-  
-  set.seed(123+10*j)
-  random.sel.lambdas <- lambdas[sample(seq_along(lambdas), ceiling(L * 0.5))]
-  L_random <- length(random.sel.lambdas)
-  
-  for(l in 1:L_random){
+for(l in 1:L_random){
     lambda <- random.sel.lambdas[l]
     # Step 1. Use the full dataset to estimate B.hat(lambda)
     if(l%%10 == 0){
@@ -352,13 +378,16 @@ for(j in 1:2){
         next} else {
           P.frob[[key]] <- norm(P[(k-1)*M + (1:M), ], "F")}
     }
-    set.seed(123+10*j)
-    thresholds <- lambda * thres.ctrl
-    random.sel.thresholds <- thresholds[sample(seq_along(thresholds), ceiling(len.t * 0.5))]
+    if(p.rand.thr == 1){
+      random.sel.thresholds <- thres.ctrl
+    }else{
+      set.seed(234+10*j)
+      random.sel.thresholds <- thres.ctrl[sample(seq_along(thres.ctrl), ceiling(len.t * p.rand.thr))]
+    }
     len.t.random <- length(random.sel.thresholds)
     
     for(ind.t in 1:len.t.random){
-      threshold <- random.sel.thresholds[ind.t]
+      threshold <- lambda * random.sel.thresholds[ind.t]
       # Step 2. finding N.hat.j according to each threhold defined by a combination of lambda  and thres.ctrl
       N.hat.jlt <- rep(FALSE, length(P.frob))
       for(n_block in 1:length(P.frob)){
@@ -391,7 +420,8 @@ for(j in 1:2){
           # Step A5: evaluate SCV for this single k
           residual <- A.Y.cv - A.X.cv %*% B.tilde # residual is n*M matrix
           emp.cov.residual <- t(residual) %*% residual / fold.k.size # empirical covariance of residual
-          SCV.single[k] <- norm(residual,"F")^2 + log(fold.k.size) * card.N.hat.jlt
+          #SCV.single[k] <- norm(residual,"F")^2 + log(fold.k.size) * card.N.hat.jlt
+          SCV.single[k] <- norm(residual,"F")^2
           
         }else{ # Circumstance B. if cardinality of N.hat is 0
           # Step B3: slicing out kth fold as CV set, the rest as training set
@@ -410,32 +440,34 @@ for(j in 1:2){
       SCV.mat[l, ind.t] <- mean(SCV.single)
     } # end of all thresholds (epsilon) 1:len.t
   } # end of lambda 1:L
-  scv.min <- which(SCV.mat == min(SCV.mat), arr.ind=T)
-  index.optimal <- scv.min[dim(scv.min)[1], ] # there could be multiple. Take the last one
-  l.optimal <- index.optimal[1]
-  ind.t.optimal <- index.optimal[2]
-  lambda.optimal <- random.sel.lambdas[l.optimal]
-  t.optimal <- thres.ctrl[ind.t.optimal]
+scv.min <- which(SCV.mat == min(SCV.mat), arr.ind=T)
+index.optimal <- scv.min[dim(scv.min)[1], ] # there could be multiple. Take the last one
+l.optimal <- index.optimal[1]
+ind.t.optimal <- index.optimal[2]
+lambda.optimal <- random.sel.lambdas[l.optimal]
+t.optimal <- random.sel.thresholds[ind.t.optimal]
+cat("Oprimal lambda: ", lambda.optimal, "\n")
+cat("Oprimal thershold: ", t.optimal, "\n")
   
-  cat("Computing optimal neighboors \n")
-  grp.lasso.result <- tryCatch({
+cat("Computing optimal neighboors \n")
+grp.lasso.result <- tryCatch({
     ADMM.grplasso.two.groups(A.X = A.X, A.Y = A.Y, d = d_out[[j]],
                              lambda=lambda.optimal, rho.init=1,
                              P.in=P, Q.in=Q, U.in=U,
                              tol.abs = tol.abs, tol.rel = tol.rel,
                              maxiter = 400)
-  }, error = function(e) {
-    message("Error in ADMM.grplasso.two.groups: ", e$message)
-    return(NULL) })
-  P <- grp.lasso.result$P
-  Q <- grp.lasso.result$Q
-  U <- grp.lasso.result$U
+}, error = function(e) {
+message("Error in ADMM.grplasso.two.groups: ", e$message)
+return(NULL) })
+P <- grp.lasso.result$P
+Q <- grp.lasso.result$Q
+U <- grp.lasso.result$U
   
   
-  n_blocks <- nrow(P)/M
-  # Process the estimated P into a neighborhood selection vector
-  P.frob <- list()
-  for(k in 1:n_blocks){
+n_blocks <- nrow(P)/M
+# Process the estimated P into a neighborhood selection vector
+P.frob <- list()
+for(k in 1:n_blocks){
     key <- unique(groups[(k-1)*M + (1:M)])
     if(length(key) > 1){
       message("Error in group definition for block ", k)
@@ -444,21 +476,20 @@ for(j in 1:2){
       }
   }
   
-  N.hat.optimal <- rep(FALSE, length(P.frob))
-  for(n_block in 1:length(P.frob)){
+N.hat.optimal <- rep(FALSE, length(P.frob))
+for(n_block in 1:length(P.frob)){
     if(!(is.null(P.frob[[n_block]]))){
-      if(P.frob[[n_block]] > t.optimal){
+      threshold <- t.optimal*lambda.optimal
+      if(P.frob[[n_block]] > threshold){
         N.hat.optimal[n_block] <- TRUE} 
     }
   }
-  if(j == p){N.hat.optimal <- c(N.hat.optimal,FALSE)}
-  cat("Optimal neighboors of node ", j, ":\n")
-  cat(N.hat.optimal)
-  cat("\n Computational time of: ")
-  cat( Sys.time() - time.start )
-  G.mat[j,] <- N.hat.optimal 
-  cat("\n")
-}
+if(j == p){N.hat.optimal <- c(N.hat.optimal,FALSE)}
+cat("Optimal neighboors of node ", j, ":\n")
+cat(N.hat.optimal)
+cat("\n Computational time of: ")
+cat( Sys.time() - time.start )
+full_result_path = paste(output_path, name_output, "_", j, ".rda", sep ="")
+save(N.hat.optimal, file =full_result_path)
+cat("Output saved to: ", full_result_path,"\n" )
 
-save(G.mat, file = paste(save.path,"res_0204_sequential.RData", sep="/"))
-Sys.time() - time.start.tot
